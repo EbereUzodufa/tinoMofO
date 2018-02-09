be911022ea6fe02ac986c089b0d4adf929a334e0
Ebere Uzodufa <ebereuzodufa@outlook.com>
2018-02-09 08:58:44 +0100

    Elements

---
+++ tinoModaFuka/tinoModaFuka.Windows/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tinoModaFuka.Models;
+
+namespace tinoModaFuka.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<Person> PersonList { get; set; }
+
+        public class TestList
+        {
+            public string name { get; set; }
+            public int age { get; set; }
+        }
+        
+        public List<TestList> TesterLists { get; set; }
+
+        public MainViewModel()
+        {
+
+            #region Note
+            //Difficult is increased in each step
+            #endregion
+            #region Test 1
+            //PersonList = new List<Person>
+            //{
+            //    new Person
+            //    {
+            //        Name="James",
+            //        Age=24
+            //    },
+            //    new Person
+            //    {
+            //        Name="LeBron",
+            //        Age=33
+            //    },
+            //    new Person
+            //    {
+            //        Name="King",
+            //        Age=54
+            //    }
+            //}; 
+            #endregion
+
+            #region Second Test
+            //PersonList = new List<Person>();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    PersonList.Add(new Person
+            //    {
+            //        Name="Person " + i.ToString(),
+            //        Age=i
+            //    });
+            //} 
+            #endregion
+
+            #region Test 3
+
+            TesterLists = new List<TestList>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                TesterLists.Add(new TestList
+                {
+                    name = "Uii " + i.ToString(),
+                    age = i
+                });
+            }
+
+            PersonList = new List<Person>();
+
+            foreach (var item in TesterLists)
+            {
+                PersonList.Add(new Person
+                {
+                    Name = "Jamii " + item.name,
+                    Age = item.age
+                });
+            } 
+            #endregion
+
+        }
+
+    }
+}
---
+++ tinoModaFuka/tinoModaFuka.Windows/ViewModels/ParameterCommand.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Input;
+//using tinoModaFuka.Models;
+
+//namespace tinoModaFuka.ViewModels
+//{
+//    public class ParameterCommand 
+//    {
+//        public Book Book { get; set; }
+
+//        public ParameterCommand(Book book)
+//        {
+//            this.Book = book;
+//        }
+//        public event EventHandler CanExecuteChanged;
+
+//        public bool CanExecute(object parameter)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Execute(object parameter)
+//        {
+//            this.Book.ParameterMethod(parameter as Book);
+//        }
+//    }
+//}
